d1a04ed1922daf405633655771369d0faf4602d3
facindito <facinditto@gmail.com>
2022-09-26 20:55:48 -0300

    fix qr generate

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,99 @@
-import { useRef } from 'react'
+import { useState } from 'react'
+import qr from 'qrcode'
 import Form from './components/forms'
-
-import useQR from './hooks/useQR'
+import { useCallback } from 'react'
 
 export default function App() {
-  const svgRef = useRef()
+  const [currentTab, setCurrentTab] = useState('text')
+  const [url, setUrl] = useState('')
+  const [qrCode, setQrCode] = useState()
 
-  const { setUrl, loading } = useQR({ svgRef })
+  const generateQR = useCallback(() => {
+    qr.toDataURL(
+      url,
+      {
+        type: 'image/webp',
+        color: {
+          light: '#fff',
+          dark: '#000',
+        },
+        width: 1080,
+        scale: 10,
+      },
+      (err, svgString) => {
+        if (err) return console.log(err)
+        setQrCode(svgString)
+        console.log(svgString)
+      }
+    )
+  }, [url])
+
+  const handleClick = (e) => {
+    setCurrentTab(e.target.name)
+  }
 
   return (
     <>
-      <header className='w-full bg-white mb-4'>
+      <header className='mb-4 w-full bg-white'>
         <div className='container mx-auto'>
           <h1 className='text-center text-4xl font-bold'>QR Generator</h1>
         </div>
       </header>
-      <div className='min-h-screen grid place-content-center'>
-        <div className='w-80 flex flex-col p-4 gap-4 rounded-lg'>
-          <article className={loading ? 'hidden' : 'p-4 bg-white rounded-md '}>
-            <div ref={svgRef}></div>
-          </article>
-          <article className='flex justify-center items-center gap-4 overflow-hidden bg-white rounded-md p-4'>
-            <Form setUrl={setUrl} />
+      <div className='container mx-auto grid min-h-screen place-content-center overflow-hidden'>
+        <div className='flex gap-4 rounded-lg p-4'>
+          {qrCode && (
+            <article className='rounded-md bg-white p-4 '>
+              <div className='w-80'>
+                <img src={qrCode} alt='' className='object-cover' />
+              </div>
+              <a
+                href={qrCode}
+                download={'QR-Code'}
+                className='inline-block rounded-md border-2 border-transparent bg-blue-600 px-4 py-2 font-semibold text-white transition ease-linear hover:border-2 hover:border-blue-600 hover:bg-white hover:text-blue-600'
+              >
+                Download
+              </a>
+            </article>
+          )}
+
+          <article className='flex flex-col items-start justify-between gap-4 overflow-hidden rounded-md bg-white p-4'>
+            <div className='flex w-full flex-wrap items-center justify-center gap-4 dark:bg-gray-800  dark:text-gray-100 sm:justify-center'>
+              <button
+                name='url'
+                onClick={handleClick}
+                className='flex flex-shrink-0 items-center border-b-4 px-5 py-2 hover:border-green-400'
+              >
+                URL
+              </button>
+              <button
+                name='email'
+                onClick={handleClick}
+                className='flex flex-shrink-0 items-center border-b-4 px-5 py-2 hover:border-green-400'
+              >
+                EMAIL
+              </button>
+              <button
+                name='sms'
+                onClick={handleClick}
+                className='flex flex-shrink-0 items-center border-b-4 px-5 py-2 hover:border-green-400 '
+              >
+                SMS
+              </button>
+              <button
+                name='text'
+                onClick={handleClick}
+                className='flex flex-shrink-0 items-center border-b-4 px-5 py-2 hover:border-green-400'
+              >
+                TEXT
+              </button>
+            </div>
+            <Form setUrl={setUrl} url={url} />
+            <button
+              className='inline-block rounded-md border-2 border-transparent bg-green-600 px-4 py-2 font-semibold text-white transition ease-linear hover:border-2 hover:border-green-600 hover:bg-white hover:text-green-600'
+              onClick={generateQR}
+            >
+              Generate QR
+            </button>
           </article>
         </div>
       </div>
--- src/components/forms/index.jsx
+++ src/components/forms/index.jsx
@@ -1,31 +1,16 @@
-import { useState } from 'react'
-
-export default function Form({ setUrl }) {
-  const [keyword, setKeyword] = useState()
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    setUrl(keyword)
-  }
-
+export default function Form({ url, setUrl }) {
   const handleChange = (e) => {
-    setKeyword(e.target.value.trim())
+    setUrl(e.target.value)
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        <textarea
-          className='appearance-none border border-gray-300 py-2 px-4 bg-white text-gray-700 placeholder-gray-400 rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent resize-none'
-          id='text'
-          placeholder='Enter your text'
-          name='text'
-          onChange={handleChange}
-        ></textarea>
-      </label>
-      <button className='block border px-4 py-2 rounded-md bg-blue-400'>
-        Generate QR
-      </button>
-    </form>
+    <textarea
+      className='my-4 h-full w-full resize-none rounded-lg border border-gray-300 py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-green-600'
+      id='text'
+      name='text'
+      placeholder='Enter your text'
+      value={url}
+      onChange={handleChange}
+    ></textarea>
   )
 }
